350afab0b6a8377a0219b92331ccc9b2bbe4e9b4
le4TC <anton.christenson@gmail.com>
2021-02-04 01:46:22 +0100

    resizing, score counts

--- graphics.js
+++ graphics.js
@@ -1,11 +1,6 @@
 function setup() {
-    createCanvas(1000, 900)
     textAlign(CENTER, CENTER)
-    strokeWeight(2)
-    textSize(20)
-
-    R = 30
-    stackR = R*sqrt(3)/2*0.9
+    
     ellipseMode(RADIUS)
 
     let boardString = getURLParams().board
@@ -16,23 +11,47 @@ function setup() {
         board = new Board(8)
     }
 
-    
-
-    L = new Layout(Layout.pointy, new Point(R, R), new Point(width/2, height/2))
+    windowResized()
 
     createButton('6').mousePressed(() => {
         board = new Board(6)
+        windowResized()
     })
     createButton('8').mousePressed(() => {
         board = new Board(8)
+        windowResized()
     })
     createButton('10').mousePressed(() => {
         board = new Board(10)
+        windowResized()
     })
     createButton('Link').mousePressed(() => {
         history.pushState({}, '', '?board=' + board.toString())
     })
-    influence = createCheckbox('Influence', false)
+    influence = createCheckbox('Influence', getItem('influence') || false).changed(() => {
+        storeItem('influence', influence.checked())
+    })
+
+    redScoreP = createP()
+    whiteScoreP = createP()
+
+    updateScores()
+}
+
+function updateScores() {
+    let redScore = 0
+    let whiteScore = 0
+    for (let H of board.hexes) {
+        if (H.color == 1) redScore ++
+        else if (H.color == -1) whiteScore ++
+        else if (H.height == 0) {
+            if (H.los[1] > H.los[-1]) redScore ++
+            if (H.los[-1] > H.los[1]) whiteScore ++
+        }
+    }
+
+    redScoreP.elt.innerHTML = 'Red score: ' + redScore
+    whiteScoreP.elt.innerHTML = 'White score: ' + whiteScore
 }
 
 stackColors = {
@@ -61,10 +80,14 @@ function draw() {
                 let center = L.hexToPixel(H)
                 let corners = L.polygonCorners(H)
                 if (H.height) {
-                    fill(hexColors[H.color])
-                    if (H.color && H.los[-H.color] > H.height) {
-                        fill(stackColors[-H.color])
-                    }
+                    // fill(hexColors[H.color])
+                    // if (H.color && H.los[-H.color] > H.height) {
+                    //     fill(stackColors[-H.color])
+                    // }
+                    if (H.color) fill(hexColors[H.color])
+                    else fill(50)
+                    // fill(50)
+                    
                 } else {
                     if (H.los[1] == 0 && H.los[-1] == 0) {
                         fill(50)
@@ -75,31 +98,37 @@ function draw() {
                 }
                 
                 if (!influence.checked()) fill('#EAC185')
+                stroke('black')
                 beginShape()
                 for (let corner of corners) {
                     vertex(corner.x, corner.y)
                 }
                 endShape(CLOSE)
-
-                if (H.height) {
-                    fill(stackColors[H.color])
-                    circle(center.x, center.y, stackR)
-                    textSize(40)
-                    fill('black')
-                    text(H.height, center.x, center.y+1)
-                } else {
-                    // fill('black')
-                    // textSize(20)
-                    // fill('red')
-                    // text(H.los[1], center.x, center.y-14)
-                    // fill('white')
-                    // text(H.los[-1], center.x, center.y+16)
-                }
-                
             }
             
         }
     }
+    push()
+    for (let q = -N+1; q < N; q ++) {
+        for (let r = -N+1; r < N; r ++) {
+            let s = -q-r
+            if (abs(s) < N) {
+                let H = board[q][r]
+                let center = L.hexToPixel(H)
+                if (H.height) {
+                    fill(stackColors[H.color])
+                    stroke('black')
+                    // strokeWeight(4)
+                    circle(center.x, center.y, stackR)
+                    fill('black')
+                    noStroke()
+                    text(H.height, center.x, center.y+1)
+                }
+            }
+        }
+    }
+    pop()
+
     
     let H = L.pixelToHex(new Point(mouseX, mouseY)).round()
     if (board.contains(H.q, H.r, H.s)) {
@@ -108,8 +137,8 @@ function draw() {
             fill(stackColors[board.turn])
             let center = L.hexToPixel(H)
             circle(center.x, center.y, stackR)
-            textSize(40)
             fill('black')
+            noStroke()
             text(H.los[board.turn], center.x, center.y+1)
         }
     }
@@ -121,6 +150,7 @@ function mousePressed() {
         H = board[H.q][H.r]
         if (H.playableFor(board.turn)) {
             board.update(H.q, H.r, board.turn, H.los[board.turn])
+            updateScores()
             board.turn = -board.turn
         }
     }
@@ -142,5 +172,17 @@ function keyPressed() {
     if (board.contains(H.q, H.r, H.s)) {
         H = board[H.q][H.r]
         board.update(H.q, H.r, color, height)
+        updateScores()
     }
+}
+
+function windowResized() {
+    let Rx = windowWidth/(2*sqrt(3)*board.size)
+    let Ry = windowHeight/(3*board.size)
+    R = min(Rx, Ry)
+    resizeCanvas(R*2*sqrt(3)*board.size, R*3*board.size)
+    stackR = R*sqrt(3)/2*0.9
+    textSize(R)
+    strokeWeight(1 + (R > 30))
+    L = new Layout(Layout.pointy, new Point(R, R), new Point(width/2, height/2))
 }
